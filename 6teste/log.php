32b0640343d4a36a035ad41d802a0653649a685d
Wade Mcfild <wademcfild@gmail.com>
2023-05-30 09:48:42 -0300

    up

--- 6teste/log.php
+++ 6teste/log.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
+    <title>bem-vindo</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
@@ -11,11 +11,14 @@
 </head>
 <body>
     <main>
-        <h1>cadastro</h1>
+        <h1>bem-vindo</h1>
     <?php
     $nome = $_GET["nome"];
     $senha = $_GET["senha"];
+
+    echo "bem-vindo $nome"
     ?>
     </main>
 </body>
+<p><a href="javascript:history.go(-1)">voltar a página anterior</a></p>
 </html>
